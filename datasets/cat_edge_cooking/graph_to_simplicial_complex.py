fbd954aa6dd2310aebddbb66d64411917630217c
Adarsh Shah Tushar <adashah21998@gmail.com>
2022-11-13 22:33:48 +0530

    added scripts to generate simplicial complex from subgraph, init simplicial embeddings and simplicial convolution layer. simplicial convolution layer is to be tested later

--- datasets/cat_edge_cooking/graph_to_simplicial_complex.py
+++ datasets/cat_edge_cooking/graph_to_simplicial_complex.py
@@ -1,42 +1,74 @@
-from subgraph_extraction import extract_subgraph
-from process_dataset import get_dgl_graph
 import dgl
 import networkx as nx
 from functools import lru_cache
 import torch
 import os
+from hodgelaplacians import HodgeLaplacians
 
 dirname = os.path.dirname(__file__)
 
 @lru_cache()
 def _get_simplices(hyperedges_path = 'hyperedges.txt', labels_path = 'hyperedge-labels.txt'):
+    '''
+    Utility function to extract simplices and their labels
+    '''
     labels = []
     with open(os.path.join(dirname,labels_path),'r') as label_file:
         for line in label_file:
             labels.append(int(line)-1)
     simplices = []
-    with open(os.path.join(hyperedges_path),'r') as hyperedges_file:
+    with open(os.path.join(dirname, hyperedges_path),'r') as hyperedges_file:
         for line in hyperedges_file:
             simplices.append(set([ int(node)-1 for node in line.split() ]))
     return simplices, labels
 
 @torch.no_grad()
 def get_simplicial_complex(subgraph:dgl.DGLGraph, graph:dgl.DGLGraph, nx_graph:nx.Graph):
-    simplices, label = _get_simplices()
+    '''
+    The function generate simplicial complex from subgraph
+    Returns:
+    Simplicial Complex : List of Set
+    Labels : List of Int
+    '''
+    simplices, labels = _get_simplices()
     edges = torch.stack(graph.find_edges(subgraph.edata['_ID'])).permute(1,0).numpy()
     nodes = set(subgraph.ndata['_ID'].numpy())
-    simplicial_complex = []
-    labels = []
+    simplex_labels = {}
     visited = set()
     for u,v in edges:
         for simplex_index in nx_graph[u][v]['hyperedge_index']:
             if simplex_index not in visited:
                 visited.add(simplex_index)
-                simplicial_complex.append(set.intersection(nodes, simplices[simplex_index]))
-                labels.append(label[simplex_index])
-    return simplicial_complex, labels
+                simplex = frozenset(set.intersection(nodes, simplices[simplex_index]))
+                simplex_labels[simplex] = torch.zeros(20)
+                hl = HodgeLaplacians([simplex])
+                for face in hl.face_set:
+                    face = frozenset(face)
+                    if face not in simplex_labels.keys():
+                        simplex_labels[face] = torch.zeros(20)
+            hl = HodgeLaplacians([simplex])
+            for face in hl.face_set:
+                face = frozenset(face)
+                simplex_labels[face][labels[simplex_index]] = 1
+            simplex_labels[simplex][labels[simplex_index]] = 1
+    return simplex_labels
 
-graph, nx_graph = get_dgl_graph()
-subgraph = extract_subgraph([5930,3243,3671], graph, h=4, enclosing_sub_graph=True, max_nodes_per_hop=500)
-simplicial_complex, labels = get_simplicial_complex(subgraph, graph, nx_graph)
-import pdb; pdb.set_trace()
+@torch.no_grad()
+def get_embeddings(simplex_labels, dim=4):
+    '''
+    This function is used to obtain initial face embeddings.
+    Parameters:
+    simplex_labels : Dict where key is face and value is embedding tensor
+    Returns:
+    embeddings : List of embedding tensor corresponding to each dimension
+    '''
+    simplicial_complex = HodgeLaplacians(simplex_labels.keys())
+    embeddings = []
+    for i in range(dim):
+        H = []
+        for face in simplicial_complex.n_faces(i):
+            face = frozenset(face)
+            H.append(simplex_labels[face])
+        embeddings.append(torch.stack(H))
+    return embeddings
+
